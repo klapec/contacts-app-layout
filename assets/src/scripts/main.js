4b99f4427f45b7e0cf156c17a058e1bbea51424c
Andrzej Kłapeć <andrzej.klapec@gmail.com>
2015-01-11 18:49:29 +0100

    json location fix

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -5,7 +5,7 @@ $(function(){
   var contactCompany = $('.contact-company');
   var tagCompany     = $('.tag-company');
   
-  $.getJSON('/contacts.json', function(data) {
+  $.getJSON('contacts.json', function(data) {
     $.each(data, function(key, val) {
       contactName[key].innerHTML = val.name;
       contactCompany[key].innerHTML = val.company;

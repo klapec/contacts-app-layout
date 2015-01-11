bb1ef4ab43ac4be120b6309e252e7a243b0220b7
Andrzej Kłapeć <andrzej.klapec@gmail.com>
2015-01-11 18:53:56 +0100

    url fixes once again

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,11 +1,12 @@
 $(function(){
 
-  var pathname       = window.location.pathname;
-  var contactName    = $('.contact-name');
-  var contactCompany = $('.contact-company');
-  var tagCompany     = $('.tag-company');
+  var pathname         = window.location.pathname;
+  var contactsLocation = pathname + 'contacts.json';
+  var contactName      = $('.contact-name');
+  var contactCompany   = $('.contact-company');
+  var tagCompany       = $('.tag-company');
   
-  $.getJSON('/contacts.json', function(data) {
+  $.getJSON(contactsLocation, function(data) {
     $.each(data, function(key, val) {
       contactName[key].innerHTML = val.name;
       contactCompany[key].innerHTML = val.company;
